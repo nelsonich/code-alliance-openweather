9fee365e442dd62d54035ebb922bbe87f33f05ca
Nelson Xachatryan <xachatryan_nelson@mail.ru>
2021-07-16 00:07:40 +0400

    create city name functional

--- js/script.js
+++ js/script.js
@@ -18,7 +18,45 @@ $(function() {
 			method: "GET",
 			dataType: "json",
 			success: function (response) {
-				console.log(response.weather[0]);
+				var data = response;
+				
+				$.ajax({
+					url: `https://api.codealliance.loc/api/set`,
+					method: "POST",
+					data: {name: value},
+					dataType: "json",
+					success: function (response) {
+						if (data) {
+		                    $('.name').empty();
+		                    $('.name').text(data.name);
+
+		                    $('.temp').empty();
+		                    $('.temp').text(data.main.temp);
+
+		                    $('.main').empty();
+		                    $('.main').text(data.weather[0].main);
+
+		                    $('.desc').empty();
+		                    $('.desc').text(data.weather[0].description);
+
+		                    $('.icon').empty();
+		                    let icon = $(`<img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">`)
+		                    $('.icon').append(icon);
+		                }
+					},
+					error: function (err) {
+						try {
+		                    throw(err);
+		                } catch (e) {
+		                    console.log(e);
+		                }
+		                let dataEmpty = $('.data').children();
+		                for (var i = 0; i < dataEmpty.length; i++) {
+		                    $('td').eq(i).empty();
+		                    $('td').eq(i).text('Empty Data!');
+		                }
+					}
+		    	});
 			},
 			error: function (err) {
 				console.log(err);
